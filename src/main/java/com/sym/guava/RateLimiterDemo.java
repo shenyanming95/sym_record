bc70192cf803e4ec203c4e03dea6dc6e998baf82
shenyanming <sym542569199@gmail.com>
2020-03-26 14:11:42 +0800

    guava使用

---
+++ src/main/java/com/sym/guava/RateLimiterDemo.java
@@ -0,0 +1,31 @@
+package com.sym.guava;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * guava提供的基于令牌桶算法的限流器.算法思想：一个固定容量的桶, 桶里存放着令牌（token）
+ * 桶一开始是空的, token以一个固定的速率r往桶里填充, 直到达到桶的容量,多余的令牌将会被丢弃.
+ * 每当一个请求过来时, 就会尝试从桶里移除一个令牌, 如果没有令牌的话, 请求无法通过.
+ *
+ * @author shenym
+ * @date 2020/3/6 8:29
+ */
+
+public class RateLimiterDemo {
+    public static void main(String[] args) {
+        // 创建一个1s只允许1次请求的限流器
+        RateLimiter limiter = RateLimiter.create(1);
+        // 在一个死循环中调用限流器, 会发现1s只有一个输出
+        new Thread(()->{
+            for(;;){
+                if(limiter.tryAcquire()){
+                    System.out.println("允许请求" + LocalDateTime.now());
+                }
+            }
+        }).start();
+    }
+}
